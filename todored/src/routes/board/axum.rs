ee80f373cabb479f625037aac524e1f05f098acf
Stefano Pigozzi <me@steffo.eu>
2023-08-05 22:10:39 +0200

    Remove unused `BoardRequest` import

--- todored/src/routes/board/axum.rs
+++ todored/src/routes/board/axum.rs
@@ -2,7 +2,7 @@ use axum::Extension;
 use axum::extract::{Path, WebSocketUpgrade};
 use uuid::Uuid;
 use crate::kebab::Skewer;
-use crate::routes::board::structs::{BoardAction, BoardRequest};
+use crate::routes::board::structs::{BoardAction};
 use crate::task::{Task, TaskIcon, TaskImportance, TaskPriority, TaskStatus};
 use super::ws;
 
