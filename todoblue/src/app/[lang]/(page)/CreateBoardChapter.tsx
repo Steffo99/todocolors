298aa8ccef9ab791957fd1bacfdb552c49918748
Stefano Pigozzi <me@steffo.eu>
2023-08-08 16:24:01 +0200

    Refactor parts of `RootMain` into `CreateBoardChapter` and `ExistingBoardChapter`

---
+++ todoblue/src/app/[lang]/(page)/CreateBoardChapter.tsx
@@ -0,0 +1,19 @@
+import {useTranslation} from "@/app/(i18n)/server"
+import {CreatePrivateBoardPanel} from "@/app/[lang]/(page)/CreatePrivateBoardPanel"
+import {CreatePublicBoardPanel} from "@/app/[lang]/(page)/CreatePublicBoardPanel"
+import {default as React} from "react"
+
+
+export async function CreateBoardChapter({lng}: {lng: string}) {
+	const {t} = await useTranslation(lng, "root")
+
+	return (
+		<div className={"chapter-2"}>
+			<h2>
+				{t("createBoardTitle")}
+			</h2>
+			<CreatePublicBoardPanel lng={lng}/>
+			<CreatePrivateBoardPanel lng={lng}/>
+		</div>
+	)
+}
---
+++ todoblue/src/app/[lang]/(page)/ExistingBoardChapter.tsx
@@ -0,0 +1,19 @@
+import {useTranslation} from "@/app/(i18n)/server"
+import {KnownBoardsPanel} from "@/app/[lang]/(page)/KnownBoardsPanel"
+import {StarredBoardsPanel} from "@/app/[lang]/(page)/StarredBoardsPanel"
+import {default as React} from "react"
+
+
+export async function ExistingBoardChapter({lng}: {lng: string}) {
+	const {t} = await useTranslation(lng, "root")
+
+	return (
+		<div className={"chapter-2"}>
+			<h2>
+				{t("existingBoardTitle")}
+			</h2>
+			<KnownBoardsPanel lng={lng}/>
+			<StarredBoardsPanel lng={lng}/>
+		</div>
+	)
+}
