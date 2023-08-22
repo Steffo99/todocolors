d4ac9b9cbdff27ae06eb7c6e51e22b96c55d89ee
Stefano Pigozzi <me@steffo.eu>
2023-08-22 05:08:43 +0200

    Use `DEFAULT_BOARD_STATE` in `useBoardState`

--- todoblue/src/app/[lang]/board/[board]/(api)/(state)/useBoardState.ts
+++ todoblue/src/app/[lang]/board/[board]/(api)/(state)/useBoardState.ts
@@ -3,10 +3,11 @@
 import {BoardSignal} from "@/app/[lang]/board/[board]/(api)/(signal)"
 import {boardReducer} from "@/app/[lang]/board/[board]/(api)/(state)/boardReducer"
 import {BoardState} from "@/app/[lang]/board/[board]/(api)/(state)/BoardState"
+import {DEFAULT_BOARD_STATE} from "@/app/[lang]/board/[board]/(api)/(state)/defaultBoardState"
 import {Reducer, useReducer} from "react"
 
 export function useBoardState() {
-    const [boardState, processBoardSignal] = useReducer<Reducer<BoardState, BoardSignal | null>>(boardReducer, {title: "", tasksById: {}})
+    const [boardState, processBoardSignal] = useReducer<Reducer<BoardState, BoardSignal | null>>(boardReducer, DEFAULT_BOARD_STATE)
 
     return {boardState, processBoardSignal}
 }
